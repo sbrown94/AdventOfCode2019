03cada271d3116fe05bcfb306ff905ffeccb8ca3
scott.brown <scott.brown@checkout.com>
2019-12-04 14:00:03 +0000

    day 1 pt 2

---
+++ Day1/Day1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:/AoC19/Inputs/Day1.txt";
+
+            var lines = File.ReadAllLines(path);
+
+            var total = 0;
+
+            foreach(var line in lines)
+            {
+                total += GetFuel(Int32.Parse(line));
+            }
+
+            Console.WriteLine(total);
+            Console.ReadLine();
+        }
+
+        static int GetFuel(int total, int count = 0)
+        {
+            var ans = (total / 3) - 2;
+
+            if (ans <= 0)
+                return count;
+
+            count += ans;
+
+            return GetFuel(ans, count);
+        }
+    }
+}
